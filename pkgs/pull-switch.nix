7b66565b7bd3d517548c864921dcee0be911f6ed
Maya the bee <hello@unw.re>
2025-02-16 17:35:18 +0100

    Made the pull-switch script rebase on pull

--- pkgs/pull-switch.nix
+++ pkgs/pull-switch.nix
@@ -8,6 +8,6 @@
   git = lib.getExe pkgs.git;
 in
   pkgs.writeShellScriptBin "pull-switch" ''
-    ${git} -C ${configuration} pull
+    ${git} -C ${configuration} pull --rebase
     sudo nixos-rebuild switch --fast --no-flake
   ''
