6da636ffdcd151242321d9827913dedc46035ead
Maya the bee <hello@unw.re>
2025-06-02 21:08:28 +0200

    bits/http: improve marginally the HTTP interface

--- bits/http.nix
+++ bits/http.nix
@@ -1,9 +1,47 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   sources = import ../sources.nix;
+
+  index = let
+    lines = config.services.yate.modules.accfile;
+  in
+    pkgs.writeTextDir "index.html" ''
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>${sources.realm}</title>
+      </head>
+      <body>
+        <code>
+          <h1><pre>${sources.banner}</pre></h1>
+
+          an experiment on building a {Nix, Yate}-based telephony system.
+
+          <h2># Routing and dial-plan</h4>
+
+          <p>
+            The routing and dial-plan is made according to the following configuration:
+            <details>
+              <summary><b>regexroute.conf</b></summary>
+              <pre>${config.services.yate.modules.regexroute}</pre>
+            </details>
+          </p>
+
+          <p>
+            The phone system also provides <b>${toString (builtins.length (builtins.attrNames lines))}</b> external connection points.
+            <ul>
+              ${lib.concatMapAttrsStringSep "\n" (name: blob: "<li><b>${name}:</b> connects to <i>${blob.server}</i>.</li>") lines}
+            </ul>
+          </p>
+        </code>
+      </body>
+      </html>
+    '';
 in {
   networking.firewall.allowedTCPPorts = [80];
 
@@ -12,31 +50,7 @@ in {
 
     document-root = pkgs.symlinkJoin {
       name = "http-source";
-      paths = [
-        (pkgs.writeTextDir "index.html" ''
-           <!DOCTYPE html>
-          <html>
-          <head>
-            <title>${sources.realm}</title>
-          </head>
-          <body>
-            <code>
-              <h1><pre>${sources.banner}</pre></h1>
-
-              an experiment on building a {Nix, Yate}-based telephony system.
-
-              <h4>Routing and dial-plan</h4>
-
-              The routing and dial-plan is made according to the following configuration:
-              <details>
-                <summary>regexroute.conf</summary>
-                <pre>${config.services.yate.modules.regexroute}</pre>
-              </details>
-            </code>
-          </body>
-          </html>
-        '')
-      ];
+      paths = [index];
     };
   };
 }
