aae19605daf23fa01700967886041dba41000155
Maya the bee <hello@unw.re>
2026-01-26 19:31:43 +0100

    zero/dect: start configuring dhcpd4

--- confs/zero/dect.nix
+++ confs/zero/dect.nix
@@ -9,4 +9,30 @@
       prefixLength = 24;
     };
   };
+
+  services.kea.dhcp4 = {
+    enable = true;
+
+    settings = {
+      interfaces-config.interfaces = ["dect0"];
+
+      lease-database = {
+        type = "memfile";
+        name = "/var/lib/kea/dhcp4.leases";
+        persist = true;
+      };
+
+      subnet4 = lib.singleton {
+        subnet = "10.127.0.0/24";
+
+        pools = lib.singleton {pool = "10.127.0.1 - 10.127.0.253";};
+        reservations = [
+          {
+            hw-address = "";
+            ip-address = "10.127.0.1";
+          }
+        ];
+      };
+    };
+  };
 }
