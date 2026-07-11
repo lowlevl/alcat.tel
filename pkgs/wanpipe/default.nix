3a280dcdd188a82eac327d693560068d2cd6b79a
Maya the bee <hello@unw.re>
2026-07-11 14:19:44 +0200

    new attempts

--- pkgs/wanpipe/default.nix
+++ pkgs/wanpipe/default.nix
@@ -1,12 +1,7 @@
 {
   lib,
   stdenv,
-  autoconf,
-  automake,
-  libtool,
-  bison,
-  flex,
-  ncurses,
+  bash,
   kernel,
 }: let
   version = "7.0.38";
@@ -16,28 +11,36 @@ in
     name = "wanpipe-${version}-${kernel.version}";
 
     hardeningDisable = ["pic"];
-    nativeBuildInputs = kernel.moduleBuildDependencies ++ [autoconf automake libtool bison flex ncurses.dev];
-    enableParallelBuilding = true;
+    nativeBuildInputs = kernel.moduleBuildDependencies ++ [bash];
+    # ++ [autoconf automake libtool bison flex ncurses.dev];
+    # enableParallelBuilding = true;
 
     src = builtins.fetchurl {
       url = "https://ftp.sangoma.com/linux/current_wanpipe/wanpipe-${version}.tgz";
       inherit sha256;
     };
 
-    preConfigure = "patchShebangs ./patches";
+    preConfigure = ''
+      patchShebangs ./patches
+    '';
 
     patches = [
-      ./00-fix-class_create-call.patch
-      ./01-fix-ktimer-size-determination.patch
-      ./02-fix-proto_ops-struct-def.patch
-      # ./03-fix-unsafe-printf-uses.patch
-      ./04-disable-faulty-ncurses-check.patch
-      ./05-missing-header-defs.patch
+      ./00-rename-EXTRA_FLAGS-to-ccflags-y.patch
+      ./01-rename-del_timer-to-timer_delete.patch
+      ./02-fix-class_create-call.patch
 
-      ./tes.patch
+      # ./00-fix-class_create-call.patch
+      # ./01-fix-ktimer-size-determination.patch
+      # ./02-fix-proto_ops-struct-def.patch
+      # # ./03-fix-unsafe-printf-uses.patch
+      # ./04-disable-faulty-ncurses-check.patch
+      # ./05-missing-header-defs.patch
+      #
+      # ./tes.patch
     ];
 
     makeFlags = [
+      "VERBOSE=1"
       "KVER=${kernel.modDirVersion}"
       "KMOD=${kernel.dev}/lib/modules/${kernel.modDirVersion}"
     ];
