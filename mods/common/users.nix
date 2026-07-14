1f7a2fa5a96333e17d532ce89622023070ee1801
Maya the bee <hello@unw.re>
2026-07-14 14:12:50 +0200

    users: add n07070's ssh key

--- mods/common/users.nix
+++ mods/common/users.nix
@@ -12,6 +12,9 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEac1v6+SJ9gZ208cyORTeD0U3/HcC5ovyvxJnH797o3 unix@socket"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApk7ff1eG79YR6emIzR+iyXchjFlh1HdTK9Uki9YmN2 bee@hive"
+
+        # n07070: co-admin for event
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzzj4cxcDMhlajkYp+HKvgKH4+QsQUtOrxgHvsU3U+Q n07070@croizat"
       ];
     };
   };
